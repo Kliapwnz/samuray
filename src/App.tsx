530872bda4a5fb960b391fbd87b0b065b548f5ab
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-22 14:49:13 +0300

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-import {Technologies} from "./components/Technologies";
-import {Header} from "./components/Header";
-import {Footer} from "./components/Footer";
+
 
 function App() {
     return (
         <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+
         </div>
     );
 }
