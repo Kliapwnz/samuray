6824d27e12e443c36dc76433251aa6bf9f4f983f
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-22 14:04:39 +0300

    2 components

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,31 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
 
+const Header = () => {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">News Feed</a>
+            <a href="#s">Messages</a>
+        </div>
+    )
+}
+const Technologies = () => {
+    return(
+        <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
+    )
+}
 export default App;
