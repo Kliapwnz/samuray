7746a3c63517449ac1226840010764b032235d6c
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-22 15:22:55 +0300

    ava and posts

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,12 @@ function App() {
                     <img
                         src={"https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"}/>
                 </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                </div>
             </div>
         </div>
     );
