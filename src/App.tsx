e88dd70a90d8ee3d1ac15935416b3b6b8a43a6c0
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-22 14:52:57 +0300

    first step for social

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ import './App.css';
 function App() {
     return (
         <div>
-
+            <header>
+                <img
+                    src={"https://www.freepnglogos.com/uploads/company-logo-png/company-logo-telestream-press-kit-12.png"}/>
+            </header>
+            <nav>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>Users</div>
+            </nav>
+            <div>Main content</div>
         </div>
     );
 }
