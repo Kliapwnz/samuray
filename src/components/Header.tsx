b1235a9ed2e0ac67b83c64fcb3c2b70482a7b471
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-22 15:32:02 +0300

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src={"https://www.freepnglogos.com/uploads/company-logo-png/company-logo-telestream-press-kit-12.png"}/>
+        </header>
+    );
+};
+
