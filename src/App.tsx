1708e8318fae11277a39834345950c67172f6b73
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-22 15:21:14 +0300

    look

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,8 @@ function App() {
             </nav>
             <div className="content">
                 <div>
-                <img src={"https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"}/>
+                    <img
+                        src={"https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"}/>
                 </div>
             </div>
         </div>
