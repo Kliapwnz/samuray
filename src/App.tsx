07e273820866eed9ff7cc277018ec5899e96e981
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-22 15:20:55 +0300

    margin and width

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,11 @@ function App() {
                 <div><a>Music</a></div>
                 <div><a>Settings</a></div>
             </nav>
-            <div className="content"><img src={"https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"}/></div>
+            <div className="content">
+                <div>
+                <img src={"https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"}/>
+                </div>
+            </div>
         </div>
     );
 }
