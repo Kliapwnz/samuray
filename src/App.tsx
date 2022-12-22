42ea7d73d3e7be1fa79cb8e42679dd34f1694f36
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-22 15:37:10 +0300

    Create and use Profile.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
 import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 
 function App() {
@@ -9,24 +10,7 @@ function App() {
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
-            <div className="content">
-                <div>
-                    <img
-                        src={"https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"}/>
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    My posts
-                    <div>New post</div>
-                </div>
-                <div>
-                    <div>Post 1</div>
-                    <div>Post 2</div>
-                    <div>Post 3</div>
-                </div>
-            </div>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src={"https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"}/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>New post</div>
+            </div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+                <div>Post 3</div>
+            </div>
+        </div>
+    );
+};
+
