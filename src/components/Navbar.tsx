0c175e79adacef06bb6a411b2571ad31d2598a0b
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-23 18:28:05 +0300

    active added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
     return (
         <nav className={n.nav}>
             <div className={n.item}><a>Profile</a></div>
-            <div className={n.item}><a>Messages</a></div>
+            <div className={`${n.item} ${n.active}`}><a>Messages</a></div>
             <div className={n.item}><a>News</a></div>
             <div className={n.item}><a>Music</a></div>
             <div className={n.item}><a>Settings</a></div>
