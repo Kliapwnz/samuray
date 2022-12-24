050fc2fcfc0e9d5905fa0663defa781a14720285
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-24 17:42:43 +0300

    add modules

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,19 +3,19 @@ import n from "./Dialogs.module.css"
 
 export const Dialogs = () => {
     return (
-        <div className="dialogs">
-            <div className="dialogs-items">
-                <div className="dialog">Anastasiya</div>
-                <div className="dialog">Elena</div>
-                <div className="dialog">Dmitriy</div>
-                <div className="dialog">Yuriy</div>
-                <div className="dialog">Natalia</div>
-                <div className="dialog">Natan</div>
+        <div className={n.dialogs}>
+            <div className={n.dialogsItems}>
+                <div className={n.dialog}>Anastasiya</div>
+                <div className={n.dialog}>Elena</div>
+                <div className={n.dialog}>Dmitriy</div>
+                <div className={n.dialog}>Yuriy</div>
+                <div className={n.dialog}>Natalia</div>
+                <div className={n.dialog}>Natan</div>
             </div>
-            <div className="messages">
-                <div className="message">Hello</div>
-                <div className="message">How are you?</div>
-                <div className="message">where are u from?</div>
+            <div className={n.messages}>
+                <div className={n.message}>Hello</div>
+                <div className={n.message}>How are you?</div>
+                <div className={n.message}>where are u from?</div>
             </div>
         </div>
     );
