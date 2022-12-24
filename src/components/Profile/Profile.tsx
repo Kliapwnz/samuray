568296e46506883e1065a0130273d1cc954ff38f
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-24 15:04:06 +0300

    app-wrapper-content

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -5,7 +5,7 @@ import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div className={n.content}>
+        <div>
             <div>
                 <img
                     src={"https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"}/>
