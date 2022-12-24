5f3f1f16657d8eda0b89aa6f83e94553dcfa579e
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-24 15:40:14 +0300

    add route

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
     return (
         <nav className={n.nav}>
             <div className={n.item}><a href="/profile">Profile</a></div>
-            <div className={n.item}><a href="/messages">Messages</a></div>
+            <div className={n.item}><a href="/dialogs">Messages</a></div>
             <div className={n.item}><a href="/news">News</a></div>
             <div className={n.item}><a href="/music">Music</a></div>
             <div className={n.item}><a href="/settings">Settings</a></div>
