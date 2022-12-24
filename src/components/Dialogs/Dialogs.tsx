1f3baebfe7a2cdc3e1ffd72fc0e4035160f64fa4
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-24 17:21:53 +0300

    add dialog items

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,16 @@ import n from "./Dialogs.module.css"
 
 export const Dialogs = () => {
     return (
-        <div>
-            dialogs
+        <div className="dialogs">
+            <div className="dialogs-items">
+                <div className="item">Anastasiya</div>
+                <div className="item">Elena</div>
+                <div className="item">Dmitriy</div>
+                <div className="item">Yuriy</div>
+                <div className="item">Natalia</div>
+                <div className="item">Natan</div>
+            </div>
+            <div className="message"></div>
         </div>
     );
 };
