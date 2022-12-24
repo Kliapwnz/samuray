57321f805bd920139f00a6441da5e3f20970b0a0
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-24 17:06:44 +0300

    active links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,11 +5,11 @@ import {NavLink} from "react-router-dom";
 export const Navbar = () => {
     return (
         <nav className={n.nav}>
-            <div className={n.item}><NavLink to="/profile">Profile</NavLink></div>
-            <div className={n.item}><NavLink to="/dialogs">Messages</NavLink></div>
-            <div className={n.item}><NavLink to="/news">News</NavLink></div>
-            <div className={n.item}><NavLink to="/music">Music</NavLink></div>
-            <div className={n.item}><NavLink to="/settings">Settings</NavLink></div>
+            <div className={n.item}><NavLink to="/profile" className = { e => e.isActive ? n.active : n.item }>Profile</NavLink></div>
+            <div className={n.item}><NavLink to="/dialogs" className = { e => e.isActive ? n.active : n.item }>Messages</NavLink></div>
+            <div className={n.item}><NavLink to="/news" className = { e => e.isActive ? n.active : n.item }>News</NavLink></div>
+            <div className={n.item}><NavLink to="/music" className = { e => e.isActive ? n.active : n.item }>Music</NavLink></div>
+            <div className={n.item}><NavLink to="/settings" className = { e => e.isActive ? n.active : n.item }>Settings</NavLink></div>
         </nav>
     );
 };
