5ecac86bd895e9b3c4d71c0be4732ea7be112fb9
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-26 14:26:22 +0300

    creating arrays

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,6 +4,22 @@ import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
 
 export const Dialogs = () => {
+
+    const dialogsData = [
+        {id: 1, name: "Anastasiya"},
+        {id: 2, name: "Elena"},
+        {id: 3, name: "Dmitriy"},
+        {id: 4, name: "Yuri"},
+        {id: 5, name: "Natali"},
+        {id: 6, name: "Natan"},
+    ]
+
+    const messagesData = [
+        {id:1 ,message:"Hello" },
+        {id:2 ,message:"How are you?" },
+        {id:3 ,message:"where are u from?" },
+    ]
+
     return (
         <div className={n.dialogs}>
             <div className={n.dialogsItems}>
@@ -11,7 +27,7 @@ export const Dialogs = () => {
                 <DialogItem name="Elena" id={2}/>
                 <DialogItem name="Dmitriy" id={3}/>
                 <DialogItem name="Yuri" id={4}/>
-                <DialogItem name="Nataliya" id={5}/>
+                <DialogItem name="Natali" id={5}/>
                 <DialogItem name="Natan" id={6}/>
             </div>
             <div className={n.messages}>
