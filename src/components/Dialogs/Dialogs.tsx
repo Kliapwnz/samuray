a3a1782cf55cc81facf5b791c372cf986374b71a
Evgeniy Klimovch <kliasanik@gmail.com>
2022-12-26 15:36:52 +0300

    export [] to index.tsx

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,27 +1,9 @@
 import React from 'react';
 import n from "./Dialogs.module.css"
-import {DialogItem} from "./DialogItem/DialogItem";
-import {Message} from "./Message/Message";
+import {dialogElements, messagesElements} from "../../index";
+
 
 export const Dialogs = () => {
-
-    const dialogs = [
-        {id: 1, name: "Anastasiya"},
-        {id: 2, name: "Elena"},
-        {id: 3, name: "Dmitriy"},
-        {id: 4, name: "Yuri"},
-        {id: 5, name: "Natali"},
-        {id: 6, name: "Natan"},
-    ]
-
-    const messages = [
-        {id: 1, message: "Hello"},
-        {id: 2, message: "How are you?"},
-        {id: 3, message: "where are u from?"},
-    ]
-
-    const dialogElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
-    const messagesElements = messages.map(m => <Message message={m.message}/>)
 
     return (
         <div className={n.dialogs}>
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {DialogItem} from "./components/Dialogs/DialogItem/DialogItem";
+import {Message} from "./components/Dialogs/Message/Message";
+
+const dialogs = [
+    {id: 1, name: "Anastasiya"},
+    {id: 2, name: "Elena"},
+    {id: 3, name: "Dmitriy"},
+    {id: 4, name: "Yuri"},
+    {id: 5, name: "Natali"},
+    {id: 6, name: "Natan"},
+]
+const messages = [
+    {id: 1, message: "Hello"},
+    {id: 2, message: "How are you?"},
+    {id: 3, message: "where are u from?"},
+]
+
+export const dialogElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+export const messagesElements = messages.map(m => <Message message={m.message}/>)
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
